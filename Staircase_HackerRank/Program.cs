885ae53086964a7088f92dc8f1e7834774c40e3e
Albert Hakobyan <abhakobyan24@gmail.com>
2021-10-27 17:00:53 +0400

    Добавьте файлы проекта.

---
+++ Staircase_HackerRank/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Staircase_HackerRank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 4;
+            staircase(n);
+
+        }
+
+        public static void staircase(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine(new string(' ', n - i) + new string('#', i));
+            }
+        }
+    }
+}
